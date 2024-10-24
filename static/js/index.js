05ad0d06a91d2a8431e975b8e3d394cc13e54dbd
Ruicheng Wang <wangrc2018cs@mail.ustc.edu.cn>
2024-10-25 03:51:45 +0800

    update video recon

--- static/js/index.js
+++ static/js/index.js
@@ -30,10 +30,9 @@ $(document).ready(function() {
     });
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
+			slidesToScroll: 3,
+			slidesToShow: 6,
 			loop: true,
-			infinite: true,
 			autoplay: false,
 			autoplaySpeed: 3000,
     }
@@ -41,22 +40,22 @@ $(document).ready(function() {
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
+    // // Loop on each carousel initialized
+    // for(var i = 0; i < carousels.length; i++) {
+    // 	// Add listener to  event
+    // 	carousels[i].on('before:show', state => {
+    // 		console.log(state);
+    // 	});
+    // }
 
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
+    // // Access to bulmaCarousel instance of an element
+    // var element = document.querySelector('#carouselVideoReconstruction');
+    // if (element && element.bulmaCarousel) {
+    // 	// bulmaCarousel instance is available as element.bulmaCarousel
+    // 	element.bulmaCarousel.on('before-show', function(state) {
+    // 		console.log(state);
+    // 	});
+    // }
 
     /*var player = document.getElementById('interpolation-video');
     player.addEventListener('loadedmetadata', function() {
